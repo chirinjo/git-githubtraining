e5de87aebb73d8e3887f4eb2f6b3af180ce27e9d
Jorge <jorge.o.chirinos@gmail.com>
2021-01-24 11:44:16 -0500

    change statement to one liner

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -8,8 +8,7 @@ function App() {
     <section className='container'>
       <h3> {people.length} birthdays today</h3>
       <List people={people}/>
-      <button onClick={() => 
-      setPeople([])} 
+      <button onClick={() => setPeople([])} 
       >clear all</button>
     </section>
   </main>
